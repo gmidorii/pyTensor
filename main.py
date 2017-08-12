6f010a4565e796a702fb2f9a860510529138a66d
tagasouichiro <stitow14@gmail.com>
2017-08-12 18:27:06 +0900

    Add variables

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
 import tensorflow as tf
 
+def placeholder():
+    node1 = tf.constant(3.0, dtype=tf.float32)
+    node2 = tf.constant(4.0)
+    print(node1, node2)
+
+    session = tf.Session()
+    print(session.run([node1, node2]))
+
+    node3 = tf.add(node1, node2)
+    print(node3)
+    print("session run(node3)", session.run(node3))
+
+    a = tf.placeholder(tf.float32)
+    b = tf.placeholder(tf.float32)
+    adder_node = a + b
+    print(session.run(adder_node, {a: 3, b: 4.5}))
+    print(session.run(adder_node, {a: [1, 3], b: [2, 5]}))
+
+    add_and_triple = adder_node * 3
+    print(session.run(add_and_triple, {a: 3, b: 4.5}))
+
+def variable():
+    sess = tf.Session()
+    W = tf.Variable([.3], dtype=tf.float32)
+    b = tf.Variable([-.3], dtype=tf.float32)
+    x = tf.placeholder(tf.float32)
+    linear_model = W * x + b
+    
+    # must initialize variables
+    init = tf.global_variables_initializer()
+    sess.run(init)
+
+    print(sess.run(linear_model, {x:[1,2,3,4]}))
+
+    y = tf.placeholder(tf.float32)
+    squared_deltas = tf.square(linear_model - y)
+    loss = tf.reduce_sum(squared_deltas)
+    print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
+
+    fixW = tf.assign(W, [-1.])
+    fixB = tf.assign(b, [1.])
+    sess.run([fixW, fixB])
+    print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
+    
+
 def main():
-  node1 = tf.constant(3.0, dtype=tf.float32)
-  node2 = tf.constant(4.0)
-  print(node1, node2)
-
-  session = tf.Session()
-  print(session.run([node1, node2]))
-
-  node3 = tf.add(node1, node2)
-  print(node3)
-  print("session run(node3)", session.run(node3))
+    placeholder()
+    variable()
+    
+    
 
 if __name__ == "__main__":
   main()

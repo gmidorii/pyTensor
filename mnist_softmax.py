e350082e8c838e2e643dd541758fe8eebc809b52
tagasouichiro <stitow14@gmail.com>
2017-08-14 16:43:47 +0900

    Finish MNIST tutorials

--- mnist_softmax.py
+++ mnist_softmax.py
@@ -20,5 +20,23 @@ y = tf.nn.softmax(tf.matmul(x, W) + b)
 y_ = tf.placeholder(tf.float32, [None, 10])
 
 # mean is like average
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indicise=[1]))
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+# train
+train_step = tf.train.GradientDescentOptimizer(0.05).minimize(cross_entropy)
+
+# create session
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+
+# TRAINING
+for _ in range(1000):
+    # 'stochastic gradient descent' training
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x:batch_xs, y_:batch_ys})
+
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
